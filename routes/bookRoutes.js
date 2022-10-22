7ba121785b13b7d8112c65dca8625a71d82363fe
Paulo Barbosa <psb2@aluno.ifal.edu.br>
2022-10-22 17:52:29 -0300

    .

--- routes/bookRoutes.js
+++ routes/bookRoutes.js
@@ -35,6 +35,7 @@ router.get('/', async (req, res) => {
     try {
         const books = await Book.find()
         res.status(200).json(books)
+        
 
     } catch (error) {
         res.status(500).json({error: error})
